4c3d6449c56d358d70b9e0cc3f6c812ab6ec5841
PPindel <przemyslaw.pindel@gmail.com>
2023-03-19 16:29:55 +0000

    Create TeamAd model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class TeamAd(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="team_ad")  # noqa E501
+    role = models.CharField(max_length=50, default='all')
+    skill_level = models.CharField(max_length=50, default='newbie')
+    updated_on = models.DateTimeField(auto_now=True)
+    description = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
