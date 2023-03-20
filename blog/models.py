cfe058ff73900e09010ec88af0c711b562791d83
PPindel <przemyslaw.pindel@gmail.com>
2023-03-20 13:33:07 +0000

    Add comment odel, update TeamAd model, migrate

--- blog/models.py
+++ blog/models.py
@@ -6,14 +6,46 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class TeamAd(models.Model):
+    ROLES = [
+        ('DMG', 'Damage Dealer'),
+        ('SUP', 'Support / Healer'),
+        ('DEF', 'Tank / Defender'),
+    ]
+    SKILL = [
+        ('NEW', 'New player'),
+        ('MID', 'Advanced player'),
+        ('EXP', 'Expert player'),
+        ('PRO', 'Proffesional player')
+    ]
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="team_ad")  # noqa E501
-    role = models.CharField(max_length=50, default='all')
-    skill_level = models.CharField(max_length=50, default='newbie')
+    role = models.CharField(max_length=3, choices=ROLES, default='ANY')
+    skill_level = models.CharField(max_length=3, choices=SKILL, default='ANY')
     updated_on = models.DateTimeField(auto_now=True)
     description = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
     excerpt = models.TextField(blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(TeamAd, on_delete=models.CASCADE, related_name='comments')  # noqa E501
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
