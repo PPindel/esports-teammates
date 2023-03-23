c8b7ff1881b2cbf698276d67bdcba53900cc1938
PPindel <przemyslaw.pindel@gmail.com>
2023-03-23 11:41:28 +0000

    Add option to view team ad adn comments

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.TeamList.as_view(), name='home')
+    path('', views.TeamList.as_view(), name='home'),
+    path('<slug:slug>/', views.TeamDetail.as_view(), name='team_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import TeamAd
 
 
@@ -8,3 +8,20 @@ class TeamList(generic.ListView):
     queryset = TeamAd.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class TeamDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = TeamAd.objects.filter(status=1)
+        team = get_object_or_404(queryset, slug=slug)
+        comments = team.comments.filter(approved=True).order_by('created_on')
+
+        return render(
+            request,
+            "team_detail.html",
+            {
+                "team": team,
+                "comments": comments,
+            },
+        )
