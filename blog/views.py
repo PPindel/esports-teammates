e47340c4519d68f552b7b4bc3c16cfb5909f5c3e
PPindel <przemyslaw.pindel@gmail.com>
2023-03-27 11:04:45 +0000

    Add option to add comments by user

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import TeamAd
+from .forms import CommentForm
 
 
 class TeamList(generic.ListView):
@@ -23,5 +24,34 @@ class TeamDetail(View):
             {
                 "team": team,
                 "comments": comments,
+                "commented": False,
+                "comment_form": CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = TeamAd.objects.filter(status=1)
+        team = get_object_or_404(queryset, slug=slug)
+        comments = team.comments.filter(approved=True).order_by('created_on')
+
+        comment_form = CommentForm(data=request.POST)
+
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = team
+            comment.save()
+        else:
+            comment_from = CommentForm()
+
+        return render(
+            request,
+            "team_detail.html",
+            {
+                "team": team,
+                "comments": comments,
+                "commented": True,
+                "comment_form": CommentForm()
             },
         )
