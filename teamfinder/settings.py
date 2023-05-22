552f2a5e223be308b85c122d8b23b54d9a92d282
PPindel <przemyslaw.pindel@gmail.com>
2023-05-22 15:25:13 +0000

    Install django extensions to create graph file

--- teamfinder/settings.py
+++ teamfinder/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sites',
+    'django_extensions',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
